3f342da6335da53f10bb3820922358c0640038fc
Amin Darestani <darestani.amin@gmail.com>
2025-02-24 13:12:50 +0330

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,644 @@
+import thirtyThreePol from './assets/Iran/33pol.jpg';
+import argeKarimKhan from './assets/Iran/argeKarimKhan.jpg';
+import Attar from './assets/Iran/Attar.jpg';
+import BagheEram from './assets/Iran/BagheEram.jpg';
+import BorjeMilad from './assets/Iran/BorjeMilad.jpg';
+import Fooman from './assets/Iran/Fooman.jpg';
+import GhaleRoodKhan from './assets/Iran/GhaleRoodKhan.jpg';
+import Hafeziye from './assets/Iran/Hafeziye.jpg';
+import kakheGolestan from './assets/Iran/kakheGolestan.jpg';
+import kalootShahdad from './assets/Iran/kalootShahdad.jpg';
+import Kish from './assets/Iran/Kish.jpg';
+import masjedeEmam from './assets/Iran/masjedeEmam.jpg';
+import MasjedNasirolmolk from './assets/Iran/MasjedNasirolmolk.jpg';
+import MeydanAzadi from './assets/Iran/MeydanAzadi.jpg';
+import MeydanNaghsheJahan from './assets/Iran/MeydanNaghsheJahan.jpg';
+import namakAbrood from './assets/Iran/namakAbrood.jpg';
+import poleKhajoo from './assets/Iran/poleKhajoo.jpg';
+import PolTabiat from './assets/Iran/PolTabiat.jpg';
+import Ramsar from './assets/Iran/Ramsar.jpg';
+import AbsharHayeKohan from './assets/Iran/AbsharHayeKohan.jpg';
+import aghaZadeh from './assets/Iran/aghaZadeh.jpg';
+import bagheShahzade from './assets/Iran/bagheShahzade.jpg';
+import baghMoozeNaderi from './assets/Iran/baghMoozeNaderi.jpg';
+import BandareBooshehr from './assets/Iran/BandareBooshehr.jpg';
+import bazarTabriz from './assets/Iran/bazarTabriz.jpg';
+import bistoon from './assets/Iran/bistoon.jpg';
+import Chaloos from './assets/Iran/Chaloos.jpg';
+import Chehelsotoon from './assets/Iran/Chehelsotoon.jpg';
+import dalkhali from './assets/Iran/dalkhali.jpg';
+import Damavand from './assets/Iran/Damavand.jpg';
+import daryacheAvan from './assets/Iran/daryacheAvan.jpg';
+import dashteDaryasar from './assets/Iran/dashteDaryasar.jpg';
+import elGoli from './assets/Iran/elGoli.jpg';
+import ferdowsi from './assets/Iran/ferdowsi.jpg';
+import GholleDamavand from './assets/Iran/GholleDamavand.jpg';
+import HaramEmamReza from './assets/Iran/HaramEmamReza.jpg';
+import jazayerNaz from './assets/Iran/jazayerNaz.jpg';
+import kakheMarmar from './assets/Iran/kakheMarmar.jpg';
+import KakheSaadabak from './assets/Iran/KakheSaadabak.jpg';
+import karizKish from './assets/Iran/karizKish.jpg';
+import kavirMesr from './assets/Iran/kavirMesr.jpg';
+import kelisayeWank from './assets/Iran/kelisayeWank.jpg';
+import khaneAbbassian from './assets/Iran/khaneAbbassian.jpg';
+import Khayyam from './assets/Iran/Khayyam.jpg';
+import Kordestan from './assets/Iran/Kordestan.jpg';
+import maghbareShoara from './assets/Iran/maghbareShoara.jpg';
+import mahmoodAbad from './assets/Iran/mahmoodAbad.jpg';
+import Masooler from './assets/Iran/Masooler.jpg';
+import meydanAmirChakhmagh from './assets/Iran/meydanAmirChakhmagh.jpg';
+import MoozeMiraseRoostaii from './assets/Iran/MoozeMiraseRoostaii.jpg';
+import MotelGhoo from './assets/Iran/MotelGhoo.jpg';
+import Pasargad from './assets/Iran/Pasargad.jpg';
+import perspolis from './assets/Iran/perspolis.jpg';
+import rangooniMasjed from './assets/Iran/rangooniMasjed.jpg';
+import roostayeAbyane from './assets/Iran/roostayeAbyane.jpg';
+import Shamsolemare from './assets/Iran/Shamsolemare.jpg';
+import tabatabai from './assets/Iran/tabatabai.jpg';
+import zarAbadKavir from './assets/Iran/zarAbadKavir.jpg';
+
+export const AVAILABLE_PLACES = [
+  {
+    id: 'p1',
+    title: 'Si-o-se-pol Bridge',
+    image: {
+      src: thirtyThreePol,
+      alt: 'A view of the historic Si-o-se-pol Bridge in Isfahan, Iran',
+    },
+    lat: 32.6546,
+    lon: 51.6677,
+  },
+  {
+    id: 'p2',
+    title: 'Khajoo Bridge',
+    image: {
+      src: poleKhajoo,
+      alt: 'A view of the historic Khajoo Bridge in Isfahan, Iran',
+    },
+    lat: 32.6361,
+    lon: 51.6707,
+  },
+  {
+    id: 'p3',
+    title: 'Imam Mosque',
+    image: {
+      src: masjedeEmam,
+      alt: 'A stunning view of Imam Mosque in Isfahan, Iran',
+    },
+    lat: 32.6581,
+    lon: 51.6771,
+  },
+  {
+    id: 'p4',
+    title: 'Naqsh-e Jahan Square',
+    image: {
+      src: MeydanNaghsheJahan,
+      alt: 'A famous UNESCO-listed historical square in Isfahan, Iran',
+    },
+    lat: 32.6573,
+    lon: 51.6776,
+  },
+  {
+    id: 'p5',
+    title: 'Golestan Palace',
+    image: {
+      src: kakheGolestan,
+      alt: 'A UNESCO World Heritage Site and historic royal palace in Tehran, Iran',
+    },
+    lat: 35.6785,
+    lon: 51.4192,
+  },
+  {
+    id: 'p6',
+    title: 'Eram Garden',
+    image: {
+      src: BagheEram,
+      alt: 'A beautiful view of Eram Garden in Shiraz, Iran',
+    },
+    lat: 29.6340,
+    lon: 52.5582,
+  },
+  {
+    id: 'p7',
+    title: 'Karim Khan Citadel',
+    image: {
+      src: argeKarimKhan,
+      alt: 'The historic Karim Khan Citadel in Shiraz, Iran',
+    },
+    lat: 29.6149,
+    lon: 52.5424,
+  },
+  {
+    id: 'p8',
+    title: 'Hafez Mausoleum',
+    image: {
+      src: Hafeziye,
+      alt: 'The tomb of the famous Persian poet Hafez in Shiraz, Iran',
+    },
+    lat: 29.6212,
+    lon: 52.5581,
+  },
+  {
+    id: 'p9',
+    title: 'Attar of Nishapur Mausoleum',
+    image: {
+      src: Attar,
+      alt: 'The tomb of the famous Persian poet Attar of Nishapur',
+    },
+    lat: 36.2133,
+    lon: 58.7961,
+  },
+  {
+    id: 'p10',
+    title: 'Nasir al-Mulk Mosque',
+    image: {
+      src: MasjedNasirolmolk,
+      alt: 'The Pink Mosque with stunning stained glass windows in Shiraz, Iran',
+    },
+    lat: 29.6092,
+    lon: 52.5431,
+  },
+  {
+    id: 'p11',
+    title: 'Milad Tower',
+    image: {
+      src: BorjeMilad,
+      alt: 'A view of the iconic Milad Tower in Tehran, Iran',
+    },
+    lat: 35.7390,
+    lon: 51.4223,
+  },
+  {
+    id: 'p12',
+    title: 'Azadi Square',
+    image: {
+      src: MeydanAzadi,
+      alt: 'A view of the historic Azadi Square in Tehran, Iran',
+    },
+    lat: 35.6990,
+    lon: 51.3381,
+  },
+  {
+    id: 'p13',
+    title: 'Pol e tabiat',
+    image: {
+      src: PolTabiat,
+      alt: 'The modern Nature Bridge in Tehran, Iran',
+    },
+    lat: 35.7340,
+    lon: 51.3814,
+  },
+  {
+    id: 'p14',
+    title: 'Fuman',
+    image: {
+      src: Fooman,
+      alt: 'A scenic view of Fuman in Gilan, Iran',
+    },
+    lat: 37.1049,
+    lon: 49.2266,
+  },
+  {
+    id: 'p15',
+    title: 'Ghale Roodkhan',
+    image: {
+      src: GhaleRoodKhan,
+      alt: 'A historical view of Roodkhan Castle in Gilan, Iran',
+    },
+    lat: 37.1412,
+    lon: 49.0605,
+  },
+  {
+    id: 'p16',
+    title: 'Ramsar',
+    image: {
+      src: Ramsar,
+      alt: 'A scenic view of Ramsar in Mazandaran, Iran',
+    },
+    lat: 36.9506,
+    lon: 50.7358,
+  },
+  {
+    id: 'p17',
+    title: 'Namak Abroud',
+    image: {
+      src: namakAbrood,
+      alt: 'A beautiful view of Namak Abroud in Mazandaran, Iran',
+    },
+    lat: 36.6450,
+    lon: 51.3372,
+  },
+  {
+    id: 'p18',
+    title: 'Kish Island',
+    image: {
+      src: Kish,
+      alt: 'A view of the famous Kish Island in Hormozgan, Iran',
+    },
+    lat: 26.5389,
+    lon: 53.9783,
+  },
+  {
+    id: 'p19',
+    title: 'Kaloute Shahdad',
+    image: {
+      src: kalootShahdad,
+      alt: 'A view of the Shahdad Kalouts desert in Kerman, Iran',
+    },
+    lat: 29.6972,
+    lon: 57.0925,
+  },
+
+  {
+    id: 'p20',
+    title: 'Ancient Waterfalls',
+    image: {
+      src: AbsharHayeKohan,
+      alt: 'A scenic view of ancient waterfalls in Iran',
+    },
+    lat: 35.6892,
+    lon: 51.3890,
+  },
+  {
+    id: 'p21',
+    title: 'Aghazadeh House',
+    image: {
+      src: aghaZadeh,
+      alt: 'A historical view of Aghazadeh House in Abarkooh, Iran',
+    },
+    lat: 31.1254,
+    lon: 53.2853,
+  },
+  {
+    id: 'p22',
+    title: 'Bagh-e Shahzadeh',
+    image: {
+      src: bagheShahzade,
+      alt: 'The beautiful Baghe Shahzadeh in Mahan, Iran',
+    },
+    lat: 30.1797,
+    lon: 57.6340,
+  },
+  {
+    id: 'p23',
+    title: 'Naderi Museum Garden',
+    image: {
+      src: baghMoozeNaderi,
+      alt: 'The garden around the Naderi Museum in Mashhad, Iran',
+    },
+    lat: 36.3033,
+    lon: 59.6069,
+  },
+  {
+    id: 'p24',
+    title: 'Bandar-e Bushehr',
+    image: {
+      src: BandareBooshehr,
+      alt: 'The port city of Bushehr, Iran, with beautiful coastal views',
+    },
+    lat: 28.9807,
+    lon: 50.8360,
+  },
+  {
+    id: 'p25',
+    title: 'Bazaar of Tabriz',
+    image: {
+      src: bazarTabriz,
+      alt: 'A vibrant view of the historic Bazaar of Tabriz in Iran',
+    },
+    lat: 38.0777,
+    lon: 46.2910,
+  },
+  {
+    id: 'p26',
+    title: 'Bistoon',
+    image: {
+      src: bistoon,
+      alt: 'The UNESCO World Heritage Site of Bistoon, Iran, with ancient inscriptions',
+    },
+    lat: 34.3880,
+    lon: 47.1363,
+  },
+  {
+    id: 'p27',
+    title: 'Chalous',
+    image: {
+      src: Chaloos,
+      alt: 'A beautiful coastal view of Chalous in northern Iran',
+    },
+    lat: 36.6172,
+    lon: 51.4934,
+  },
+  {
+    id: 'p28',
+    title: 'Chehel Sotoun Palace',
+    image: {
+      src: Chehelsotoon,
+      alt: 'The historic Chehel Sotoun Palace in Isfahan, Iran',
+    },
+    lat: 32.6450,
+    lon: 51.6499,
+  },
+  {
+    id: 'p29',
+    title: 'Jangale Dalkhali',
+    image: {
+      src: dalkhali,
+      alt: 'The traditional Dalkhali village in Iran with scenic views',
+    },
+    lat: 33.9000,
+    lon: 58.5000,
+  },
+
+  {
+    id: 'p30',
+    title: 'Damavand',
+    image: {
+      src: Damavand,
+      alt: 'The majestic Mount Damavand, the highest peak in Iran',
+    },
+    lat: 35.9511,
+    lon: 52.1094,
+  },
+  {
+    id: 'p31',
+    title: 'Daryacheh Avan',
+    image: {
+      src: daryacheAvan,
+      alt: 'A beautiful view of Daryacheh Avan, a serene lake in Iran',
+    },
+    lat: 35.5363,
+    lon: 51.6510,
+  },
+  {
+    id: 'p32',
+    title: 'Dashte Daryasar',
+    image: {
+      src: dashteDaryasar,
+      alt: 'The vast Daryasar Desert in Iran',
+    },
+    lat: 35.2167,
+    lon: 53.7917,
+  },
+  {
+    id: 'p33',
+    title: 'El Goli',
+    image: {
+      src: elGoli,
+      alt: 'The El Goli Park in Tabriz, a scenic recreational spot',
+    },
+    lat: 38.0707,
+    lon: 46.2990,
+  },
+  {
+    id: 'p34',
+    title: 'Ferdowsi',
+    image: {
+      src: ferdowsi,
+      alt: 'The tomb of Ferdowsi, a legendary poet in Iran',
+    },
+    lat: 36.4671,
+    lon: 59.5314,
+  },
+  {
+    id: 'p35',
+    title: 'Gholle Damavand',
+    image: {
+      src: GholleDamavand,
+      alt: 'Gholle Damavand, a scenic view of the surrounding area of Damavand Mountain',
+    },
+    lat: 35.9086,
+    lon: 52.1497,
+  },
+  {
+    id: 'p36',
+    title: 'Haram Emam Reza',
+    image: {
+      src: HaramEmamReza,
+      alt: 'The grand shrine of Imam Reza in Mashhad, Iran',
+    },
+    lat: 36.2828,
+    lon: 59.6081,
+  },
+  {
+    id: 'p37',
+    title: 'Jazayer Naz',
+    image: {
+      src: jazayerNaz,
+      alt: 'Jazayer Naz, a beautiful island in the Persian Gulf',
+    },
+    lat: 26.8039,
+    lon: 54.5061,
+  },
+  {
+    id: 'p38',
+    title: 'Kakhe Marmar',
+    image: {
+      src: kakheMarmar,
+      alt: 'The Marble Palace (Kakhe Marmar) in Tehran, Iran',
+    },
+    lat: 35.7356,
+    lon: 51.4020,
+  },
+  {
+    id: 'p39',
+    title: 'Kakhe Saadabad',
+    image: {
+      src: KakheSaadabak,
+      alt: 'The historical Kakhe Saadabad in Iran',
+    },
+    lat: 36.3432,
+    lon: 58.3453,
+  },
+  {
+    id: 'p40',
+    title: 'Kariz Kish',
+    image: {
+      src: karizKish,
+      alt: 'The Kariz underground city in Kish Island, Iran',
+    },
+    lat: 26.5462,
+    lon: 53.9733,
+  },
+  {
+    id: 'p41',
+    title: 'Kavir Mesr',
+    image: {
+      src: kavirMesr,
+      alt: 'The Mesr Desert in Iran, offering stunning views of the desert landscape',
+    },
+    lat: 33.5333,
+    lon: 56.0000,
+  },
+  {
+    id: 'p42',
+    title: 'Kelisaye Wank',
+    image: {
+      src: kelisayeWank,
+      alt: 'The historical Kelisaye Wank in Iran',
+    },
+    lat: 35.6200,
+    lon: 47.0721,
+  },
+  {
+    id: 'p43',
+    title: 'Khane Abbassian',
+    image: {
+      src: khaneAbbassian,
+      alt: 'The beautiful Khane Abbassian in Kashan, Iran',
+    },
+    lat: 33.9871,
+    lon: 51.5074,
+  },
+  {
+    id: 'p44',
+    title: 'Khayyam Tomb',
+    image: {
+      src: Khayyam,
+      alt: 'The tomb of the great Persian poet Khayyam in Nishapur, Iran',
+    },
+    lat: 36.2158,
+    lon: 58.7885,
+  },
+  {
+    id: 'p45',
+    title: 'Kordestan',
+    image: {
+      src: Kordestan,
+      alt: 'The beautiful mountains and landscapes of Kordestan in western Iran',
+    },
+    lat: 35.5779,
+    lon: 46.9974,
+  },
+  {
+    id: 'p46',
+    title: 'Maghbare Shoara',
+    image: {
+      src: maghbareShoara,
+      alt: 'The tomb of famous poets in Maghbare Shoara, Tabriz, Iran',
+    },
+    lat: 38.0800,
+    lon: 46.2910,
+  },
+  {
+    id: 'p47',
+    title: 'Mahmood Abad',
+    image: {
+      src: mahmoodAbad,
+      alt: 'The coastal view of Mahmood Abad in northern Iran',
+    },
+    lat: 36.7204,
+    lon: 52.4240,
+  },
+  {
+    id: 'p48',
+    title: 'Masoole',
+    image: {
+      src: Masooler,
+      alt: 'The picturesque village of Masooler in northern Iran',
+    },
+    lat: 37.2040,
+    lon: 49.1146,
+  },
+  {
+    id: 'p49',
+    title: 'Meydan Amir Chakhmagh',
+    image: {
+      src: meydanAmirChakhmagh,
+      alt: 'The historic Amir Chakhmagh Square in Yazd, Iran',
+    },
+    lat: 31.8973,
+    lon: 54.3671,
+  },
+  {
+    id: 'p50',
+    title: 'Mooze Mirase Roostaii',
+    image: {
+      src: MoozeMiraseRoostaii,
+      alt: 'The Museum of Rural Heritage in Iran',
+    },
+    lat: 34.9936,
+    lon: 50.8971,
+  },
+  {
+    id: 'p51',
+    title: 'Motel Ghoo',
+    image: {
+      src: MotelGhoo,
+      alt: 'A beautiful traditional motel in Iran',
+    },
+    lat: 36.3400,
+    lon: 58.4113,
+  },
+  {
+    id: 'p52',
+    title: 'Pasargad',
+    image: {
+      src: Pasargad,
+      alt: 'The ancient ruins of Pasargad, the first capital of the Achaemenid Empire',
+    },
+    lat: 30.2356,
+    lon: 53.1950,
+  },
+  {
+    id: 'p53',
+    title: 'Persepolis',
+    image: {
+      src: perspolis,
+      alt: 'The famous Persepolis ruins, an ancient capital of the Achaemenid Empire in Iran',
+    },
+    lat: 29.9356,
+    lon: 52.8915,
+  },
+  {
+    id: 'p54',
+    title: 'Masjede Rangooni',
+    image: {
+      src: rangooniMasjed,
+      alt: 'The beautiful Rangooni Masjed, an ancient mosque in Iran',
+    },
+    lat: 32.6400,
+    lon: 51.6890,
+  },
+  {
+    id: 'p55',
+    title: 'Roostaye Abyaneh',
+    image: {
+      src: roostayeAbyane,
+      alt: 'The historic village of Abyaneh, known for its unique architecture and culture',
+    },
+    lat: 33.4690,
+    lon: 51.2700,
+  },
+  {
+    id: 'p56',
+    title: 'Shamsolemare',
+    image: {
+      src: Shamsolemare,
+      alt: 'Shamsolemare, an ancient palace in Iran with stunning gardens',
+    },
+    lat: 32.5200,
+    lon: 53.8011,
+  },
+  {
+    id: 'p57',
+    title: 'Khane ye Tabatabai',
+    image: {
+      src: tabatabai,
+      alt: 'The beautiful Tabatabai House in Kashan, Iran',
+    },
+    lat: 33.9916,
+    lon: 51.5100,
+  },
+  {
+    id: 'p58',
+    title: 'Kavir e Zar Abad',
+    image: {
+      src: zarAbadKavir,
+      alt: 'The stunning views of Zar Abad Kavir Desert in Iran',
+    },
+    lat: 33.8000,
+    lon: 56.2000,
+  },
+
+];
